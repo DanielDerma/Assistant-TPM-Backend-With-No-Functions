397b2b279448c0db108278ffb36754820987d416
DanielDerma <danielderma94@gmail.com>
2022-11-02 23:10:48 -0600

    fix env

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const admin = require("firebase-admin");
+require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 
-const serviceAccount = require("./service.json");
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_ce: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 const isDev = process.env.NODE_ENV !== "production";
 
@@ -45,10 +57,11 @@ const validateFirebaseIdToken = async (req, res, next) => {
     return;
   }
 };
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 app.use(cors({ origin: true }));
 app.use(!isDev ? validateFirebaseIdToken : (req, res, next) => next());
