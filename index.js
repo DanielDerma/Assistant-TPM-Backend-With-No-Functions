c1cde2f9a144698880b44e20e44388dccf891f0a
DanielDerma <danielderma94@gmail.com>
2022-11-02 23:46:07 -0600

    fix2

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ const cors = require("cors");
 const app = express();
 
 const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_ce: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  type: process.env.FIREBASE_TYPE || "",
+  project_id: process.env.FIREBASE_PROJECT_ID || "",
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID || "",
+  private_key: process.env.FIREBASE_PRIVATE_KEY || "",
+  client_email: process.env.FIREBASE_CLIENT_EMAIL || "",
+  client_id: process.env.FIREBASE_CLIENT_ID || "",
+  auth_uri: process.env.FIREBASE_AUTH_URI || "",
+  token_uri: process.env.FIREBASE_TOKEN_URI || "",
+  auth_provider_x509_ce: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL || "",
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL || "",
 };
 
 const isDev = process.env.NODE_ENV !== "production";
