1a2a61d866439643587d6119cfff0b24299bad57
DanielDerma <danielderma94@gmail.com>
2022-11-03 19:26:42 -0600

    fixauth4

--- index.js
+++ index.js
@@ -73,4 +73,6 @@ app.get("/hello-world", (req, res) => {
 
 app.use(require("./routes/users.routes"));
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Server is running on port 3000")
+);
